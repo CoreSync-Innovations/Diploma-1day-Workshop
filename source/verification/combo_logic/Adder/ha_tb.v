a0cb10194d38dd786a47f45af040995848933d3f
Saransh Choudhary <scsc2699@gmail.com>
2024-08-11 02:12:23 +0530

    Update to testbench

--- source/verification/combo_logic/Adder/ha_tb.v
+++ source/verification/combo_logic/Adder/ha_tb.v
@@ -15,11 +15,13 @@ initial begin
         {a,b} = {2'b01}; #50;
         {a,b} = {2'b10}; #50;
         {a,b} = {2'b11}; #50;
-        $finish;
+     $finish;
 end
 
 
 initial begin
+            $display("\t\tTruth table of a Half Adder");
+            $display("----------------------------------------------------------------------------");
             $display("A\t\tB\t\t\tSum\t\t\tCarry");
             $monitor("%b\t\t%b\t\t\t%b\t\t\t%b",a,b,sum,carry);
 end
