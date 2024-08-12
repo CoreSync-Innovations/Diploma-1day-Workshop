f87a7ce11aa8647ed5d1938a39a5531fc95c123d
CoreSync-Innovations <coresyncinnovations@gmail.com>
2024-08-12 21:11:06 +0530

    Indentation update

--- source/rtl/combo_logic/Adder/fa.v
+++ source/rtl/combo_logic/Adder/fa.v
@@ -2,10 +2,10 @@
 module fa (
               input  wire a, 
               input  wire b,
-  			  input  wire carry_in,
+  			      input  wire carry_in,
               output wire sum, 
               output wire carry_out
-  		  );
+  		    );
 
 assign sum = a ^ b ^ carry_in;  
 assign carry_out = (a & b) + ((a ^ b) & carry_in);
